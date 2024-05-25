243b467b066d756e49df6ab851467b3dbb815119
Serhii Repinskyi <115186821+serhiirepinskyi@users.noreply.github.com>
2024-05-25 18:55:38 +0300

    solve several 8 kyu challenges

---
+++ 8kuy/Beginner-Lost-Without-a-Map.js
@@ -0,0 +1,11 @@
+// DESCRIPTION:
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+// [1, 2, 3]-- > [2, 4, 6]
+
+// SOLUTIONS
+const maps = (x) => x.map((elm) => elm * 2);
+
+// Examples
+console.log(maps([1, 2, 3])); // [2, 4, 6]
