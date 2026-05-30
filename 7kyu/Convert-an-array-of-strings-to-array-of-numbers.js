a0809c7c4a042311eb6fbc1a4c352f2587c0fb9f
Serhii Repinskyi <repinskyisv@gmail.com>
2026-05-30 23:29:23 +0300

    Solution: 7kyu - Convert an array of strings to array of numbers

---
+++ 7kyu/Convert-an-array-of-strings-to-array-of-numbers.js
@@ -0,0 +1,28 @@
+// Convert an array of strings to array of numbers
+
+// DESCRIPTION:
+// Oh no!
+// Some really funny web dev gave you a sequence of numbers from his API response as an sequence of strings!
+
+// You need to cast the whole array to the correct type.
+
+// Create the function that takes as a parameter a sequence of numbers represented as strings and outputs a sequence of numbers.
+
+// ie:["1", "2", "3"] to [1, 2, 3]
+
+// Note that you can receive floats as well.
+
+// SOLUTIONS:
+// 1
+function toNumberArray(stringarray) {
+  return stringarray.map(Number);
+}
+
+// 2
+// function toNumberArray(stringarray) {
+//   return stringarray.map((x) => +x);
+// }
+
+// EXAMPLES:
+console.log(toNumberArray(["1", "2", "3"])); // [1, 2, 3]
+console.log(toNumberArray(["1.1", "2.2", "3.3"])); // [1.1, 2.2, 3.3]
