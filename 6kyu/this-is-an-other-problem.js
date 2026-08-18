3162025aa7b07336c1e93269e2ecd138042ae415
Serhii Repinskyi <repinskyisv@gmail.com>
2026-08-18 23:37:43 +0300

    Solution: 6kyu - this is an other problem

---
+++ 6kyu/this-is-an-other-problem.js
@@ -0,0 +1,103 @@
+// "this" is an other problem
+
+// DESCRIPTION:
+
+// After you've solved @priyankaherur's problem ( http://www.codewars.com/kata/this-is-a-problem/javascript ) you may try to solve this other one.
+
+// The problem:
+// Having created a function NamedOne which takes first & last names as parameters and returns an object with firstName, lastName and fullName ( = firstName + a space + lastName ) properties which should be all accessibles, we discovered that "accessible" also means "mutable".
+
+// If, for example, we've got a "NamedOne" like this :
+
+// var namedOne = new NamedOne("Naomi","Wang")
+// namedOne.firstName // -> "Naomi"
+// namedOne.lastName  // -> "Wang"
+// namedOne.fullName  // -> "Naomi Wang"
+
+// ...properties may be changed :
+
+// namedOne.firstName = "John"
+// namedOne.firstName // -> "John"
+// namedOne.lastName = "Doe"
+// namedOne.lastName  // -> "Doe"
+
+// ...but all properties are not updated !
+
+// namedOne.fullName  // -> "Naomi Wang"
+// //-- Oh no ! we want fullName == "John Doe" now !
+
+// Your mission:
+
+// So the purpose of this kata is to create an object with accessible and "updatable" (can i say that?) properties.
+// - If .firstName or .lastName are changed, then .fullName should also be changed
+// - If .fullName is changed, then .firstName and .lastName should also be changed.
+
+// Note : "input format" to .fullName will be firstName + space+ lastName. If given fullName isn't valid then no property is changed.
+
+// Examples:
+// var namedOne = new NamedOne("Naomi","Wang")
+
+// namedOne.firstName = "John"
+// namedOne.lastName = "Doe"
+// // ...then...
+// namedOne.fullName // -> "John Doe"
+
+// // -- And :
+// namedOne.fullName = "Bill Smith"
+// // ...then...
+// namedOne.firstName // -> "Bill"
+// namedOne.lastName  // -> "Smith"
+
+// // -- But :
+// namedOne.fullName = "Tom" // -> no : lastName missing
+// namedOne.fullName = "TomDonnovan" // -> no : no space between first & last names
+// namedOne.fullName // -> "Bill Smith" (unchanged)
+
+// Can you change our function to create such a NamedOne object ?
+
+// ( Hint: in this kata you'll try to define properties of an object )
+
+// SOLUTION:
+function NamedOne(first, last) {
+  this.firstName = first;
+  this.lastName = last;
+
+  Object.defineProperty(this, "fullName", {
+    get() {
+      return `${this.firstName} ${this.lastName}`;
+    },
+
+    set(value) {
+      const names = value.split(" ");
+
+      if (names.length === 2 && names[0] && names[1]) {
+        this.firstName = names[0];
+        this.lastName = names[1];
+      }
+    },
+  });
+}
+
+// EXAMPLE:
+const namedOne = new NamedOne("John", "Doe");
+
+console.log(namedOne.firstName); // John
+console.log(namedOne.lastName); // Doe
+console.log(namedOne.fullName); // John Doe
+
+namedOne.firstName = "Jane";
+console.log(namedOne.firstName); // Jane
+console.log(namedOne.fullName); // Jane Doe
+
+namedOne.firstName = "Jane";
+namedOne.lastName = "Smith";
+console.log(namedOne.lastName); // Smith
+console.log(namedOne.fullName); // Jane Smith
+
+namedOne.fullName = "Juan Herrero";
+console.log(namedOne.firstName); // Juan
+console.log(namedOne.lastName); // Herrero
+console.log(namedOne.fullName); // Juan Herrero
+
+namedOne.fullName = "Tom";
+console.log(namedOne.fullName); // Juan Herrero
